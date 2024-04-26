a179add2632820b987ac833c88cf106e43550b4a
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-04-26 16:24:04 +0900

    스프링 시큐리티, 로그인, 로그아웃, 회원가입 1

---
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,4 @@
+package com.koreait.exam.acc_app_2024_04.app.handler;
+
+public class CustomAuthenticationFailureHandler {
+}
--- src/main/java/com/koreait/exam/acc_app_2024_04/app/initdata/InitDataBefore.java
+++
@@ -1,4 +0,0 @@
-package com.koreait.exam.acc_app_2024_04.app.initdata;
-
-public class InitDataBefore {
-}
